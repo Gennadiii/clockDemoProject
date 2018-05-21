2e31f885ab7cee445631a496115e84aeb9a92247
Gennadii_Mishchevskii <gmishchevskii@cogniance.com>
2018-05-21 08:55:28 +0300

    Added iterable object helper

--- spec/volume.spec.ts
+++ spec/volume.spec.ts
@@ -3,11 +3,6 @@ import {service} from "../testRunner";
 
 describe('Appium test', function () {
 
-  it('Gets landing page', async () => {
-    // await service.landing.skipUpdate();
-    expect(await service.landing.pageOpened()).toBeTruthy('Landing page did not get opened')
-  });
-
   it('Check volume', async () => {
     await service.landing.openMorningTab();
     await service.morning.pageOpened();
