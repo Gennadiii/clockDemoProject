e5ccc4997132fb2b41da21ff60277ed3235198a9
Gennadii_Mishchevskii <gmishchevskii@cogniance.com>
2018-05-30 14:20:20 +0300

    Synchronised element finder

--- src/generic/components/label.ts
+++ src/generic/components/label.ts
@@ -4,7 +4,6 @@ import {Component} from "./component";
 interface LabelInterface extends Component {
 }
 
-
 class Label extends Component implements LabelInterface {
 
   constructor(protected ef) {
